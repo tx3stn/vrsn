0cc50293304ce6603f59cdfe5b41bb36cf3eda07
tx3stn <14163530+tx3stn@users.noreply.github.com>
2025-05-25 13:07:11 +0100

    add other tests for check command

--- .scripts/e2e-tests/check.sh
+++ .scripts/e2e-tests/check.sh
@@ -14,15 +14,38 @@ setup() {
 
 teardown() {
 	echo "test teardown..."
+	git add .
+	git stash
+	git stash drop
 	git checkout "$main_branch"
 	git branch -D "$test_branch"
 }
 
-@test "vrsn check with VERSION file: no bump" {
+@test "vrsn check w. VERSION file: no bump" {
 	git checkout -b "$test_branch"
 	run vrsn check
 	assert_failure
 	assert_line --index 0 'was: 0.0.1'
 	assert_line --index 1 'now: 0.0.1'
 	assert_line --index 2 --partial 'version has not been bumped'
+}
+
+@test "vrsn check w. VERSION file: valid bump" {
+	git checkout -b "$test_branch"
+	echo "0.1.0" >VERSION
+	run vrsn check
+	assert_success
+	assert_line --index 0 'was: 0.0.1'
+	assert_line --index 1 'now: 0.1.0'
+	assert_line --index 2 'valid version bump'
+}
+
+@test "vrsn check w. VERSION file: invalid bump" {
+	git checkout -b "$test_branch"
+	echo "0.2.0" >VERSION
+	run vrsn check
+	assert_failure
+	assert_line --index 0 'was: 0.0.1'
+	assert_line --index 1 'now: 0.2.0'
+	assert_line --index 2 --partial 'invalid version bump'
 }
